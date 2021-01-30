f6b3cd7217e6425cc0db6c89740f84c0483315bc
Panagiotis Konstantinou <panknst@gmail.com>
2021-01-30 14:18:06 +0000

    First commit

---
+++ src/main/java/AmazonPs5.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.util.List;
+
+public class AmazonPs5 {
+
+    static SafariDriver safariDriver = new SafariDriver();
+
+    public static void main(String[] args) {
+        boolean found = false;
+        safariDriver.navigate().to("https://www.amazon.co.uk/PlayStation-9395003-5-Console/dp/B08H97NYGP/ref=nav_signin?dchild=1&keywords=ps5&qid=1609447873&sr=8-1&th=1");
+        System.out.print("At ps5 page");
+        int refreshCount = 10;
+        List<WebElement> element = safariDriver.findElements(By.id("add-to-cart-button"));
+        System.out.println("Found add to cart button");
+        for (int i = 0; i < refreshCount; i++) {
+            if (element.size() > 0) {
+
+                // Do the operation here on the element
+            } else {
+                safariDriver.navigate().refresh();
+                System.out.println("Refresh count: " + i);
+            }
+        }
+    }
+}
