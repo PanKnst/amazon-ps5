9d03b4183c4493ae2673cb55e8eb72359a862a07
Panagiotis Konstantinou <panknst@gmail.com>
2021-02-14 17:09:34 +0000

    Replaced with springboot app

---
+++ src/main/java/com/amazon/ps5/Ps5Application.java
@@ -0,0 +1,13 @@
+package com.amazon.ps5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ps5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ps5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/amazon/ps5/Ps5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazon.ps5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ps5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
